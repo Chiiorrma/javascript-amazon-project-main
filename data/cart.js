98ecc47f8f60775a240ecc4230b020d0743ff802
Chiiorrma <chiumuo76@gmail.com>
2024-10-02 10:00:24 -0700

    got the value for quantity selected

--- data/cart.js
+++ data/cart.js
@@ -9,6 +9,9 @@ export const cart = [{
 export function addToCart(productId) {
   let matchingItem;
 
+  const quantitySelector = document.querySelector('js-quantity-selector-${product.id}');
+  let quantity = quantitySelector.value;
+
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
